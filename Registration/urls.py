d2a418adad267d73619ac8f3853ab865f7295780
Rakshith Vikramaj <rv.g6598@outlook.com>
2017-03-04 23:49:23 +0530

    Delete View

--- Registration/urls.py
+++ Registration/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     url(r'^events/(?P<pk>[0-9]+)/$', EventsDetail.as_view(), name='eventdetail'),
     url(r'participant/(?P<pk>[0-9]+)/$', ParticipantDetail.as_view(), name='participant_detail'),
     url(r'participant/update/(?P<pk>[0-9]+)/$', ParticipantUpdate.as_view(), name='participant_update'),
+    url(r'participant/delete/(?P<pk>[0-9]+)/$',Participant_del.as_view(), name='participant_delete'),
     url(r'^participant/update/success$', updatesuccesspage, name='update_success')
 
 ]
