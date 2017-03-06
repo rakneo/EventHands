2f5f02249a34d0f245f47970f9ae37a9f401eb07
Rakshith Vikramaj <rv.g6598@outlook.com>
2017-03-06 15:33:51 +0530

    Revert "Settings Page Added"
    This reverts commit d1c5ce460b50d74ec1417b06371da1bdd4b9b962.

--- Registration/context_processors.py
+++
@@ -1,8 +0,0 @@
-from Registration.models import SiteConfig
-
-
-def siteconfigs(request):
-    all_configs = SiteConfig.objects.all()
-    return {
-        'configs': all_configs,
-    }
--- cienciafest/settings.py
+++ cienciafest/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 import dj_database_url
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -74,7 +75,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'Registration.context_processors.siteconfigs',
             ],
         },
     },
